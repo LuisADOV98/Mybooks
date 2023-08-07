8e5a714c095d27fdb2e6bd4d6ef843a5840193eb
LuisADOV98 <luisadov26@gmail.com>
2023-08-07 17:37:14 +0200

    final dia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo:"home", pathMatch:'full'},
+  {path: "header", component: HeaderComponent},
+  {path: "footer", component: FooterComponent},
+  {path: "home", component: HomeComponent}, 
+  {path: "form-register", component: FormRegisterComponent},
+  {path: "register", component: RegisterComponent},
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
