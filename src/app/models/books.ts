5e395550b8c765fddad02926cfdbd8e01dec319d
LuisADOV98 <luisadov26@gmail.com>
2023-08-10 16:23:21 +0200

    final dia 3

--- src/app/models/books.ts
+++ src/app/models/books.ts
@@ -7,7 +7,7 @@ export class Books {
     public price: number;
     public photo: string;
 
-    constructor(id_book:number,id_user:number,title:string,type:string,author:string,price:number,photo:string){
+    constructor(id_book:number=0,id_user:number=0,title:string,type:string,author:string,price:number,photo:string){
 
         this.id_book = id_book
         this.id_user = id_user
